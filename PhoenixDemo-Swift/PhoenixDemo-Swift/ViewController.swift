6441e312c1f07af2ac61b79085378f04dd67e72a
Rui Silvestre <rui.silvestre@tigerspike.com>
2015-07-20 15:01:13 +0100

    added SDK workspace + demo app projects for Swift and ObjectiveC

---
+++ PhoenixDemo-Swift/PhoenixDemo-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhoenixDemo-Swift
+//
+//  Created by Rui Silvestre on 20/07/2015.
+//  Copyright © 2015 Tigerspike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
