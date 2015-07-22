f9ccd4ce77fee81ec797c6ae42c7e487e49b245b
Josep Rodriguez Lopez <josep.rodriguez@tigerspike.com>
2015-07-22 10:32:39 +0100

    PSDK-2 [REF] Refactored phoenix onfiguration class to improve readability, externalised tasks to extensions PSDK-2 #time 10m

--- PhoenixSDK/PhoenixSDK/PhoenixConfiguration.swift
+++ PhoenixSDK/PhoenixSDK/PhoenixConfiguration.swift
@@ -8,40 +8,51 @@
 
 import Foundation
 
-/**
- This class holds the data to configure the phoenix SDK. It provides initialisers to
-read the configuration from a plist file, and allows.
-*/
+/// This class holds the data to configure the phoenix SDK. It provides initialisers to
+/// read the configuration from a plist file, and allows.
 public class PhoenixConfiguration
 {
-    private static let clientIdKey = "phoenix_client_id";
-    private static let clientSecretKey = "phoenix_client_secret";
-    private static let phoenixProjectIdKey = "phoenix_project_id";
-    private static let phoenixApplicationIdKey = "phoenix_application_id";
-    private static let phoenixRegionKey = "phoenix_region";
-    private static let phoenixEnvironmentKey = "phoenix_environment";
-    
+
     /// The client ID
     public var clientId:String!;
     
     /// The client secret
     public var clientSecret:String!;
+
+}
+
+/// Extension to provide copy initializer
+public extension PhoenixConfiguration {
     
-    /// Default initializer. Does nothing.
-    public init() {
-        
+    convenience public init(copying:PhoenixConfiguration){
+        self.init()
+        self.clientId = copying.clientId
+        self.clientSecret = copying.clientSecret
     }
+    
+}
+
+
+/// This extension encapsulates the file reading and parsing in the configuration
+public extension PhoenixConfiguration {
+    
+    // Constants to load from a config file.
+    private static let clientIdKey = "client_id"
+    private static let clientSecretKey = "client_secret"
+    private static let phoenixProjectIdKey = "project_id"
+    private static let phoenixApplicationIdKey = "application_id"
+    private static let phoenixRegionKey = "region"
     
     /// Initialises the configuration with a plist with the file name specified in the main
     convenience public init (fromFile fileName:String!,inBundle bundle:NSBundle!) throws {
         self.init();
-        try readFromFile(fileName, inBundle:bundle);
+        try readFromFile(fileName, inBundle:bundle)
     }
     
     /// Reads the given file in the main bundle into the configuration.
     public func readFromFile(fileName:String!, inBundle bundle:NSBundle!) throws {
         if let plistResourcePath = bundle.pathForResource(fileName, ofType: "plist") {
-            readFromPlistPath(plistResourcePath);
+            readFromPlistPath(plistResourcePath)
         }
         throw PhoenixGenericErrors.NoSuchConfigFile
     }
@@ -50,14 +61,15 @@ public class PhoenixConfiguration
         if let plistData = NSDictionary(contentsOfFile: plistResourcePath) {
             
             if plistData[PhoenixConfiguration.clientIdKey] != nil {
-                self.clientId = String(plistData[PhoenixConfiguration.clientIdKey]!);
+                self.clientId = String(plistData[PhoenixConfiguration.clientIdKey]!)
             }
             
             if plistData[PhoenixConfiguration.clientSecretKey] != nil {
-                self.clientSecret = String(plistData[PhoenixConfiguration.clientSecretKey]!);
+                self.clientSecret = String(plistData[PhoenixConfiguration.clientSecretKey]!)
             }
         }
         
         return false;
     }
+
 }
