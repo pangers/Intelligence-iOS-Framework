60fab60fe5c2d0de544d547858b1d329b98b0543
Josep Rodriguez Lopez <josep.rodriguez@tigerspike.com>
2015-07-23 09:32:46 +0100

    PSDK-2 [ADD] Adding breathing space PSDK-2 #time 10m

--- PhoenixSDK/PhoenixSDK/PhoenixRegion.swift
+++ PhoenixSDK/PhoenixSDK/PhoenixRegion.swift
@@ -9,8 +9,10 @@
 import Foundation
 
 public extension Phoenix {
+    
     /// An enum with the regions to which the SDK can be pointing to.
     public enum Region {
+        
         /// US Region
         case UnitedStates
         
@@ -26,22 +28,32 @@ public extension Phoenix {
         /// - Returns: String to the base url to use (including protocol).
         func baseURL() -> String {
             switch (self) {
-            case .UnitedStates: return "https://api.phoenixplatform.com"
-            case .Australia:    return "https://api.phoenixplatform.com.au"
-            case .Europe:       return "https://api.phoenixplatform.eu"
-            case .Singapore:    return "https://api.phoenixplatform.com.sg"
+            case .UnitedStates:
+                return "https://api.phoenixplatform.com"
+            case .Australia:
+                return "https://api.phoenixplatform.com.au"
+            case .Europe:
+                return "https://api.phoenixplatform.eu"
+            case .Singapore:
+                return "https://api.phoenixplatform.com.sg"
             }
         }
         
+        /// - Returns: The region that a specific code represents. Can return nil if the
+        /// code does not match any region.
         static func fromString(str: String) -> Region? {
             switch str {
-            case "US": return .UnitedStates
-            case "AU": return .Australia
-            case "EU": return .Europe
-            case "SG": return .Singapore
-            default:   return nil
+            case "US":
+                return .UnitedStates
+            case "AU":
+                return .Australia
+            case "EU":
+                return .Europe
+            case "SG":
+                return .Singapore
+            default:
+                return nil
             }
         }
     }
 }
-    
