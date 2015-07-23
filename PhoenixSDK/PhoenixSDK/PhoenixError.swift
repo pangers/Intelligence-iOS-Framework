c7012b3dd18d36ff1970dc51c1f4cd08b1306c62
Josep Rodriguez Lopez <josep.rodriguez@tigerspike.com>
2015-07-23 09:33:52 +0100

    PSDK-2 [ADD] Added extra test and spacing PSDK-2 #time 10m

--- PhoenixSDK/PhoenixSDK/PhoenixError.swift
+++ PhoenixSDK/PhoenixSDK/PhoenixError.swift
@@ -11,12 +11,16 @@ import Foundation
 // TODO: Improve these descriptions.
 /// Errors that can occur during Configuration.
 public enum ConfigurationError: Int, ErrorType {
+    
     /// Configuration file does not exist.
     case FileNotFoundError
+    
     /// Property is invalid.
     case InvalidPropertyError
+    
     /// Configuration file is in incorrect format.
     case InvalidFileError
+    
     /// Property is missing.
     case MissingPropertyError
 }
