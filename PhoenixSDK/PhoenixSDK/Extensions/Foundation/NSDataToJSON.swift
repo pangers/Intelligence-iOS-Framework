57aaadab04474c2c9cd8202164670ce14d1ea096
Josep Rodriguez Lopez <josep.rodriguez@tigerspike.com>
2015-07-28 10:32:24 +0100

    PSDK-7 [ADD] Added comments to NSData extension. Also, changed type alias of JSONArray to AnyObject, since it can contain other objects apart from dictionaries. PSDK-7 #time 10m

--- PhoenixSDK/PhoenixSDK/Extensions/Foundation/NSDataToJSON.swift
+++ PhoenixSDK/PhoenixSDK/Extensions/Foundation/NSDataToJSON.swift
@@ -8,10 +8,16 @@
 
 import Foundation
 
-typealias JSONArray = [JSONDictionary]
-typealias JSONDictionary = [String: AnyObject]
+/// Alias for an array loaded from a JSON object.
+public typealias JSONArray = [AnyObject]
+
+/// Alias for a dictionary loaded from a JSON object.
+public typealias JSONDictionary = [String: AnyObject]
+
 
 extension NSData {
+    
+    /// Returns: Any object, as an optional, as returned from NSJSONSerialization.JSONObjectWithData
     private func tryJSON() -> AnyObject? {
         do {
             return try NSJSONSerialization.JSONObjectWithData(self, options: NSJSONReadingOptions.AllowFragments)
@@ -22,13 +28,13 @@ extension NSData {
     }
     
     /// - Returns: Array of JSONDictionary objects or nil if cast fails.
-    var jsonArray: JSONArray? {
+    public var jsonArray: JSONArray? {
         guard let arr = tryJSON() as? JSONArray else { return nil }
         return arr
     }
     
     /// - Returns: A JSONDictionary object or nil if cast fails.
-    var jsonDictionary: JSONDictionary? {
+    public var jsonDictionary: JSONDictionary? {
         guard let dict = tryJSON() as? JSONDictionary else { return nil }
         return dict
     }
