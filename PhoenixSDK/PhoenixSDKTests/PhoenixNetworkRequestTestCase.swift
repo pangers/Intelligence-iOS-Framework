c3ef41a61accb373a209ddf047c028136f458f18
Josep Rodriguez Lopez <josep.rodriguez@tigerspike.com>
2015-07-30 08:41:07 +0100

    PSDK-7 [ADD] Added OHHTPStubs and a unit test for the network class PSDK-7 #time 10m

---
+++ PhoenixSDK/PhoenixSDKTests/PhoenixNetworkRequestTestCase.swift
@@ -0,0 +1,82 @@
+//
+//  PhoenixNetworkRequest.swift
+//  PhoenixSDK
+//
+//  Created by Josep Rodriguez on 29/07/2015.
+//  Copyright © 2015 Tigerspike. All rights reserved.
+//
+
+import XCTest
+import OHHTTPStubs
+
+@testable import PhoenixSDK
+
+class PhoenixNetworkRequestTestCase : XCTestCase {
+    
+    var phoenix:Phoenix?
+    var configuration:Phoenix.Configuration?
+    
+    override func setUp() {
+        do {
+            try self.configuration = PhoenixSDK.Phoenix.Configuration(fromFile: "config", inBundle:NSBundle(forClass: PhoenixNetworkRequestTestCase.self))
+            self.configuration!.region = .Europe
+            try self.phoenix = Phoenix(withConfiguration: configuration!)
+        }
+        catch {
+            // I'm happy.
+        }
+        
+    }
+    
+    func testTokenObtained() {
+        let expectation = expectationWithDescription("")
+        
+        // Swift
+        OHHTTPStubs.stubRequestsPassingTest(
+            { request in
+                print("")
+                print("")
+                print("")
+                print("")
+                print("")
+                print("")
+                print("")
+                print("")
+                print(request)
+                print("")
+                print("")
+                print("")
+                print("")
+                print("")
+                print("")
+                print("")
+                print("")
+                
+                return request.URL!.absoluteString.hasPrefix(self.configuration!.baseURL!.absoluteString) &&
+                    request.HTTPMethod == "POST"
+            }
+            ,
+            withStubResponse: { _ in
+                let stubData = "{\"access_token\":\"OTJ1a2tyeGZrMzRqM2twdXZ5ZzI4N3QycmFmcWp3ZW0=\",\"token_type\":\"bearer\",\"expires_in\":7200}".dataUsingEncoding(NSUTF8StringEncoding)
+                return OHHTTPStubsResponse(data: stubData!, statusCode:200, headers:nil)
+        })
+        
+        XCTAssert(!self.phoenix!.isAuthenticated, "Phoenix is authenticated before a response")
+
+        self.phoenix!.tryLogin({ (data, response, error) -> () in
+            print(data)
+            print(response)
+            print(error)
+            expectation.fulfill()
+        })
+        
+        
+        waitForExpectationsWithTimeout(10) { (error:NSError?) -> Void in
+            XCTAssertNil(error,"Error in expectation")
+            
+            XCTAssert(self.phoenix!.isAuthenticated, "Phoenix is not authenticated after a successful response")
+        }
+    }
+    
+}
+
