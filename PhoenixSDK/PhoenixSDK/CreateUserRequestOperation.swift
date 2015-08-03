acdc653ddf6d57716463d2578a03a93544312e9a
Josep Rodriguez Lopez <josep.rodriguez@tigerspike.com>
2015-08-03 17:32:31 +0100

    PSDK-35 [MOD] Implemented the create user request operation PSDK-35 #time 10m

--- PhoenixSDK/PhoenixSDK/CreateUserRequestOperation.swift
+++ PhoenixSDK/PhoenixSDK/CreateUserRequestOperation.swift
@@ -11,10 +11,13 @@ import Foundation
 class CreateUserRequestOperation : PhoenixNetworkRequestOperation {
     
     private(set) var createdUser:PhoenixUser?
+    let configuration:PhoenixConfigurationProtocol
+    
     
     /// Default initializer with all required parameters
     init(session:NSURLSession, user:PhoenixUser, authentication:Phoenix.Authentication, configuration:PhoenixConfigurationProtocol) {
         let request = NSURLRequest.phx_httpURLRequestForCreateUser(user, configuration: configuration)
+        self.configuration = configuration
         super.init(withSession: session, withRequest: request, withAuthentication: authentication)
     }
     
@@ -22,9 +25,25 @@ class CreateUserRequestOperation : PhoenixNetworkRequestOperation {
     override func main() {
         super.main()
         
-        // self.error and output contain the error and output. Parse output into createdUser.
-        print(self.output)
+        if error != nil {
+            return
+        }
         
+        if let output = self.output, data = output.data {
+            
+            do {
+                let jsonResponse:JSONDictionary = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments) as! JSONDictionary
+                
+                if let data = jsonResponse["Data"] as? JSONArray where data.count > 0 {
+                    let userData = data[0] as! JSONDictionary
+                    self.createdUser = Phoenix.User(withJSON: userData, withConfiguration:configuration)
+                }
+            }
+            catch {
+                // TODO: Error parsing the response
+                
+            }
+        }
     }
 
 }
