47c4847bafd2cfa3602dc1241b52a35d05d82381
Josep Rodriguez Lopez <josep.rodriguez@tigerspike.com>
2015-08-03 12:52:12 +0100

    PSDK-35 [DOC] Documented Phoenix users and restructured the project. PSDK-35 #time <10m>

--- PhoenixSDK/PhoenixSDK/PhoenixUser.swift
+++ PhoenixSDK/PhoenixSDK/PhoenixUser.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+// MARK: the constants of the JSON keys.
 private let idKey = "Id"
 private let usernameKey = "Username"
 private let passwordKey = "Password"
@@ -21,25 +22,42 @@ private let isActiveKey = "IsActive"
 private let metadataKey = "MetaData"
 private let userTypeKey = "UserTypeId"
 
+/// The user types that the SDK supports
 public enum UserType : String {
     
+    /// Regular user
     case User = "User"
     
 }
 
+/// A protocol defining the Phoenix Users behaviour.
 public protocol PhoenixUser {
     
+    /// The user id. Non modifiable. The implementer should use a let.
     var userId:Int { get }
+    
+    /// The company id. Non modifiable. Should be fetched from the Configuration of Phoenix.
     var companyId:String {get}
     
+    /// The username
     var username:String {get set}
+    
+    /// The password
     var password:String {get set}
+    
+    /// The firstname
     var firstName:String {get set}
+    
+    /// The lastname
     var lastName:String {get set}
+    
+    /// The avatar URL
     var avatarURL:String {get set}
     
 }
 
+/// Extends phoenix user to provide the variables that should be disregarded by the user.
+/// Also provides a toJSON method to return a JSON dictionary from the values of the user.
 extension PhoenixUser {
     
     var lockingCount:Int {
@@ -80,20 +98,39 @@ extension PhoenixUser {
     }
 }
 
+
 extension Phoenix {
-    
+
+    /// The user class implementation
     public class User : PhoenixUser {
         
+        /// The user Id as a let
         public let userId:Int
+        
+        /// The company Id as a let.
         public let companyId:String
         
+        /// the username
         public var username:String
+        
+        /// The password
         public var password:String
+        
+        /// The first name
         public var firstName:String
+        
+        /// The last name
         public var lastName:String
+        
+        /// The avatar url
         public var avatarURL:String
         
-        /// Parses the JSON dictionary and returns the data read.
+        /// Parses the JSON dictionary to create the User object. If it fails to
+        /// parse all values, it will return nil.
+        ///
+        /// - Parameters:
+        ///     - withJSON: The json dictionary as obtained from the backend.
+        ///     - withConfiguration: The configuration that holds the company Id.
         init?(withJSON json:JSONDictionary, withConfiguration configuration:Configuration) {
             self.username = ""
             self.password = ""
