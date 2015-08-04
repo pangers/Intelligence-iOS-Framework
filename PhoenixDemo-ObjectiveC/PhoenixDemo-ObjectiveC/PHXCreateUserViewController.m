bce2d1da4dd4a3bde84f76fe4a173864acd47b45
Josep Rodriguez Lopez <josep.rodriguez@tigerspike.com>
2015-08-04 10:14:28 +0100

    PSDK-35 [ADD] Added the create user view controller PSDK-35 #time 10m

---
+++ PhoenixDemo-ObjectiveC/PhoenixDemo-ObjectiveC/PHXCreateUserViewController.m
@@ -0,0 +1,33 @@
+//
+//  PHXCreateUserViewController.m
+//  PhoenixDemo-ObjectiveC
+//
+//  Created by Josep Rodriguez on 04/08/2015.
+//  Copyright © 2015 Tigerspike. All rights reserved.
+//
+
+#import "PHXCreateUserViewController.h"
+
+@interface PHXCreateUserViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *username;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+@property (weak, nonatomic) IBOutlet UITextField *firstName;
+@property (weak, nonatomic) IBOutlet UITextField *lastName;
+@property (weak, nonatomic) IBOutlet UITextField *avatarURL;
+
+
+@end
+
+@implementation PHXCreateUserViewController
+
+- (IBAction)didTapCreateUser:(id)sender {
+    NSString* username = self.username.text
+    NSString* password = self.password.text
+    NSString* firstname = self.firstName.text
+    NSString* lastname = self.lastName.text
+    NSString* avatarURL = self.avatarURL.text
+    
+}
+
+@end
