239dd95edd48016a4e6af409321ea3373ce0d4a6
Chris Nevin <chris.nevin@alivemobile.com>
2015-08-05 17:47:21 +0100

    PSDK-45 [COM] Added comments.

--- PhoenixSDK/PhoenixSDK/PhoenixUserRequestOperation.swift
+++ PhoenixSDK/PhoenixSDK/PhoenixUserRequestOperation.swift
@@ -8,9 +8,14 @@
 
 import Foundation
 
+/// Base class for User request operations. Inherits from PhoenixNetworkRequestOperation.
 class PhoenixUserRequestOperation : PhoenixNetworkRequestOperation {
+    
+    /// User will be set if response is parsable.
     var user: Phoenix.User?
+    /// Configuration is required, and will be set on init.
     var configuration: PhoenixConfigurationProtocol?
+    /// Error will be populated using this error code, subclassses to set error code at appropriate times.
     var errorCode: Int = 0
     
     /// The operation will run synchronously the data task and store the error and output.
