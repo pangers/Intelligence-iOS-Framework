04a9b0c320770a177cde72ae857449b57bac817b
Josep Rodriguez Lopez <josep.rodriguez@tigerspike.com>
2015-10-06 09:20:52 +0100

    PSDK-148 [ADD] Added the same UI to the objective c project as the swift one.

---
+++ PhoenixDemo-ObjectiveC/PhoenixDemo-ObjectiveC/PHXLocationModuleViewController.m
@@ -0,0 +1,37 @@
+//
+//  PHXLocationModuleViewController.m
+//  PhoenixDemo-ObjectiveC
+//
+//  Created by Josep Rodriguez on 06/10/2015.
+//  Copyright © 2015 Tigerspike. All rights reserved.
+//
+
+#import "PHXLocationModuleViewController.h"
+
+@interface PHXLocationModuleViewController ()
+
+@end
+
+@implementation PHXLocationModuleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
