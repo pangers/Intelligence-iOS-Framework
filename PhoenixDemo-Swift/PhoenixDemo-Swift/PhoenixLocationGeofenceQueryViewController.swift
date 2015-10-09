b931f35c0885d9a7cc01e6487f928596c3cf8f53
Josep Rodriguez Lopez <josep.rodriguez@tigerspike.com>
2015-10-09 12:04:56 +0100

    PSDK-148 [MOD] Updated to use a picker view to select the required sorting criteria.

--- PhoenixDemo-Swift/PhoenixDemo-Swift/PhoenixLocationGeofenceQueryViewController.swift
+++ PhoenixDemo-Swift/PhoenixDemo-Swift/PhoenixLocationGeofenceQueryViewController.swift
@@ -23,8 +23,12 @@ class PhoenixLocationGeofenceQueryViewController: UIViewController {
     @IBOutlet weak var pageSizeText: UITextField!
     @IBOutlet weak var pageText: UITextField!
     @IBOutlet weak var radiusText: UITextField!
+    @IBOutlet weak var sortByText: UITextField!
     @IBOutlet weak var sortDirectionSegmentedControl: UISegmentedControl!
-    
+
+    @IBOutlet var sortByPickerView: UIPickerView!
+    @IBOutlet var accessoryView: UIView!
+
     // MARK:- Properties
     var latitude:Double?
     var longitude:Double?
@@ -35,14 +39,22 @@ class PhoenixLocationGeofenceQueryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: Selector("didTapOnView")))
+        sortByText.inputView = sortByPickerView
+
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: Selector("resignResponders")))
         
         self.latitudeText.text = latitude != nil ? String(latitude!) : ""
         self.longitudeText.text = longitude != nil ? String(longitude!) : ""
+        self.sortByText.text = "Distance"
+        
+        // Set accessory view to all texts:
+        [latitudeText, longitudeText, pageSizeText, pageText, radiusText, sortByText].forEach {
+            $0.inputAccessoryView = accessoryView
+        }
     }
     
-    func didTapOnView() {
-        [latitudeText, longitudeText, pageSizeText, pageText, radiusText].forEach{
+    func resignResponders() {
+        [latitudeText, longitudeText, pageSizeText, pageText, radiusText, sortByText].forEach{
             $0.resignFirstResponder()
         }
     }
@@ -50,6 +62,8 @@ class PhoenixLocationGeofenceQueryViewController: UIViewController {
     // MARK:- IBActions
     
     @IBAction func didTapSave(sender: AnyObject) {
+        resignResponders()
+        
         let query = GeofenceQuery(location: PhoenixCoordinate(withLatitude: Double(latitudeText.text ?? "0") ?? 0, longitude: Double(longitudeText.text ?? "0") ?? 0))
         query.radius = Double(radiusText.text ?? "1000")
         query.pageSize = Int(pageSizeText.text ?? "10")
@@ -61,6 +75,53 @@ class PhoenixLocationGeofenceQueryViewController: UIViewController {
     }
     
     @IBAction func didTapCancel(sender: AnyObject) {
+        resignResponders()
         dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
+// MARK:- Sort by picker view datasourve
+extension PhoenixLocationGeofenceQueryViewController : UIPickerViewDataSource {
+
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 6
+    }
+}
+
+extension PhoenixLocationGeofenceQueryViewController : UIPickerViewDelegate {
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch row {
+        case GeofenceSortCriteria.Distance.rawValue:
+            return "Distance"
+            
+        case GeofenceSortCriteria.Id.rawValue:
+            return "Id"
+            
+        case GeofenceSortCriteria.Reference.rawValue:
+            return "Reference"
+            
+        case GeofenceSortCriteria.Name.rawValue:
+            return "Name"
+            
+        case GeofenceSortCriteria.Description.rawValue:
+            return "Description"
+            
+        case GeofenceSortCriteria.Address.rawValue:
+            return "Address"
+            
+        default:
+            assert(false,"Should never have a row above the number of sort criteria")
+        }
+        
+        return ""
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        sortByText.text = self.pickerView(pickerView, titleForRow: row, forComponent: component)
     }
 }
