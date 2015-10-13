c8f502b09069cf0a235c519db035d59fac9d2358
Chris Nevin <chris.nevin@alivemobile.com>
2015-10-13 11:08:14 +0100

    PSDK-96 [TST] Removed dead code.

--- PhoenixSDK/PhoenixLocationModuleSDKTests/PhoenixLocationBaseTestCase.swift
+++ PhoenixSDK/PhoenixLocationModuleSDKTests/PhoenixLocationBaseTestCase.swift
@@ -22,7 +22,6 @@ class PhoenixLocationBaseTestCase : PhoenixBaseTestCase, PhoenixInternalDelegate
     override func setUp() {
         super.setUp()
         mockLocationManager = MockCLLocationManager()
-        
         location = LocationModule(withDelegate: self, network: mockNetwork, configuration: mockConfiguration, locationManager: LocationManager(locationManager:mockLocationManager))
     }
     
@@ -32,48 +31,6 @@ class PhoenixLocationBaseTestCase : PhoenixBaseTestCase, PhoenixInternalDelegate
         OHHTTPStubs.removeAllStubs()
         location = nil
         mockLocationManager = nil
-    }
-    
-    // MARK: URL Mock
-    
-    func mockResponseForURL(url:NSURL!, method:String?, response:(data:String?,statusCode:HTTPStatusCode,headers:[String:String]?), expectation: XCTestExpectation? = nil, callback:MockCallback? = nil) {
-        mockResponseForURL(url, method: method, responses: [response], callbacks: [callback], expectations: [expectation])
-    }
-    
-    func mockResponseForURL(url:NSURL!, method:String?, responses:[MockResponse], callbacks: [MockCallback?]? = nil, expectations:[XCTestExpectation?]? = nil) {
-        let count = responses.count
-        var runs = [(MockCallback?, MockResponse, XCTestExpectation)]()
-        for i in 0..<count {
-            runs += [ (callbacks?[i], responses[i], expectations?[i] ??
-                expectationWithDescription("mock \(url) iteration \(i)")) ]
-        }
-        OHHTTPStubs.stubRequestsPassingTest(
-            { request in
-                if let method = method where method != request.HTTPMethod {
-                    return false
-                }
-                return request.URL! == url
-            },
-            withStubResponse: { _ in
-                let (callback, response, expectation) = runs.first!
-                runs.removeAtIndex(0)
-                // Execute callback before fulfilling expectation so we can chain multiple expectations together
-                callback?()
-                expectation.fulfill()
-                let stubData = ((response.data) ?? "").dataUsingEncoding(NSUTF8StringEncoding)!
-                return OHHTTPStubsResponse(
-                    data: stubData,
-                    statusCode: Int32(response.statusCode.rawValue),
-                    headers:response.headers)
-        })
-    }
-    
-    // MARK: - Authentication Mock
-
-    func mockOAuth() -> MockOAuthProvider {
-        let oauth = MockOAuthProvider()
-        oauth.fakeAccessToken(oauth.sdkUserOAuth)
-        return oauth
     }
     
     // MARK:- PhoenixInternalDelegate
