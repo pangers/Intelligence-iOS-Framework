ba1907cab2bb9583fd9bc7eb703d5967e3765fdf
Michael Lake <michael.lake@tigerspike.com>
2016-02-09 11:29:03 +0000

    PSDK-221 - Updated sample apps to support RevokeRole - Rearrange sample apps so that all actions on a specific user are done after login

---
+++ PhoenixDemo-ObjectiveC/PhoenixDemo-ObjectiveC/PHXManageUserViewController.m
@@ -0,0 +1,176 @@
+//
+//  PHXManageUserViewController.m
+//  PhoenixDemo-ObjectiveC
+//
+//  Created by Michael Lake on 09/02/2016.
+//  Copyright © 2016 Tigerspike. All rights reserved.
+//
+
+#import "PHXManageUserViewController.h"
+#import "AppDelegate.h"
+#import "PHXViewUserViewController.h"
+
+static NSString * const PHXUpdateUserSegue = @"UpdateUser";
+static NSString * const PHXUnwindOnLogoutSegue = @"UnwindOnLogout";
+
+@interface PHXManageUserViewController ()
+
+@end
+
+@implementation PHXManageUserViewController
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:PHXUpdateUserSegue]) {
+        PHXViewUserViewController *viewUser = segue.destinationViewController;
+        viewUser.user = self.user;
+    }
+    else if ([segue.identifier isEqualToString:PHXUnwindOnLogoutSegue]) {
+    }
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    UIApplication *application = UIApplication.sharedApplication;
+    AppDelegate *delegate = application.delegate;
+    
+    if (indexPath.section == 0) {
+        if (indexPath.row == 0) {
+            [self registerDeviceToken];
+        }
+        else if (indexPath.row == 1) {
+            [self unregisterDeviceToken];
+        }
+        else {
+            [delegate alertWithMessage:@"Unexpected Row"];
+        }
+    }
+    else if (indexPath.section == 1) {
+        if (indexPath.row == 0) {
+            [self assignRole];
+        }
+        else if (indexPath.row == 1) {
+            [self revokeRole];
+        }
+        else {
+            [delegate alertWithMessage:@"Unexpected Row"];
+        }
+    }
+    else if (indexPath.section == 2) {
+        if (indexPath.row == 0) {
+            // Segue in IB will handle this
+        }
+        else {
+            [delegate alertWithMessage:@"Unexpected Row"];
+        }
+    }
+    else if (indexPath.section == 3) {
+        if (indexPath.row == 0) {
+            [self logout];
+        }
+        else {
+            [delegate alertWithMessage:@"Unexpected Row"];
+        }
+    }
+    else {
+        [delegate alertWithMessage:@"Unexpected Section"];
+    }
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (void)registerDeviceToken {
+    UIApplication *application = UIApplication.sharedApplication;
+    
+    NSInteger tokenId = [[NSUserDefaults standardUserDefaults] integerForKey:PhoenixDemoStoredDeviceTokenKey];
+    
+    if (tokenId != 0) {
+        AppDelegate *delegate = application.delegate;
+        [delegate alertWithMessage:@"Already Registered!"];
+    } else {
+        [application registerForRemoteNotifications];
+        [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert categories:nil]];
+    }
+}
+
+- (void)unregisterDeviceToken {
+    UIApplication *application = UIApplication.sharedApplication;
+    AppDelegate *delegate = application.delegate;
+    
+    NSInteger tokenId = [[NSUserDefaults standardUserDefaults] integerForKey:PhoenixDemoStoredDeviceTokenKey];
+    
+    if (tokenId == 0) {
+        [delegate alertWithMessage:@"Not Registered!"];
+        return;
+    }
+    
+    [[[PHXPhoenixManager phoenix] identity] unregisterDeviceTokenWithId:tokenId callback:^(NSError * _Nullable error) {
+        BOOL notRegisteredError = [IdentityErrorDomain rangeOfString:error.domain].location != NSNotFound && error.code == IdentityErrorDeviceTokenNotRegisteredError;
+        
+        if (error != nil && !notRegisteredError) {
+            [delegate alertWithError: error];
+        } else {
+            [[NSUserDefaults standardUserDefaults] removeObjectForKey: PhoenixDemoStoredDeviceTokenKey];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+            
+            [delegate alertWithMessage: @"Unregister Succeeded!"];
+        }
+    }];
+}
+
+- (void)assignRole {
+    UIApplication *application = UIApplication.sharedApplication;
+    AppDelegate *delegate = application.delegate;
+    
+    [delegate alertWithMessage:@"Not implemented yet"];
+}
+
+- (void)revokeRole {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Enter Details"
+                                                                             message:nil
+                                                                      preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"RoleId";
+    }];
+    
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Cancel"
+                                                        style:UIAlertActionStyleCancel
+                                                      handler:^(UIAlertAction * _Nonnull action) {
+                                                      }]];
+    
+    __weak typeof(self) weakSelf = self;
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Revoke Role"
+                                                        style:UIAlertActionStyleDefault
+                                                      handler:^(UIAlertAction * _Nonnull action) {
+                                                          
+                                                          NSString *roleId = alertController.textFields.firstObject.text;
+                                                          
+                                                          [PHXPhoenixManager.phoenix.identity revokeRole:[roleId integerValue] user:weakSelf.user callback:^(PHXUser * _Nullable user, NSError * _Nullable error) {
+                                                              dispatch_async(dispatch_get_main_queue(), ^{
+                                                                  UIApplication *application = UIApplication.sharedApplication;
+                                                                  AppDelegate *delegate = application.delegate;
+                                                                  
+                                                                  if (error != nil) {
+                                                                      [delegate alertWithMessage:[NSString stringWithFormat:@"Failed with error: %@", @(error.code)]];
+                                                                  }
+                                                                  else {
+                                                                      [delegate alertWithMessage:@"Role Revoked!"];
+                                                                  }
+                                                              });
+                                                          }];
+                                                      }]];
+    
+    [self presentViewController:alertController animated:true completion:nil];
+}
+
+- (void)logout {
+    [PHXPhoenixManager.phoenix.identity logout];
+    
+    self.user = nil;
+    
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey: PhoenixDemoStoredDeviceTokenKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    [self performSegueWithIdentifier:PHXUnwindOnLogoutSegue sender:self];
+}
+
+@end
---
+++ PhoenixDemo-Swift/PhoenixDemo-Swift/ManageUserViewController.swift
@@ -0,0 +1,174 @@
+//
+//  ManageUserViewController.swift
+//  PhoenixDemo-Swift
+//
+//  Created by Michael Lake on 09/02/2016.
+//  Copyright © 2016 Tigerspike. All rights reserved.
+//
+
+import UIKit
+import PhoenixSDK
+
+class ManageUserViewController : UITableViewController {
+    private let UpdateUserSegue = "UpdateUser"
+    private let UnwindOnLogoutSegue = "UnwindOnLogout"
+    
+    var user: Phoenix.User? = nil
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == UpdateUserSegue {
+            if let updateUser = segue.destinationViewController as? ViewUserViewController {
+                updateUser.user = user
+            }
+        }
+        else if segue.identifier == UnwindOnLogoutSegue {
+            
+        }
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        defer {
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        }
+        
+        let application = UIApplication.sharedApplication()
+        let delegate = application.delegate as! AppDelegate
+        
+        if indexPath.section == 0 {
+            if indexPath.row == 0 {
+                registerDeviceToken()
+            }
+            else if indexPath.row == 1 {
+                unregisterDeviceToken()
+            }
+            else {
+                delegate.alert(withMessage: "Unexpected Row")
+            }
+        }
+        else if indexPath.section == 1 {
+            if indexPath.row == 0 {
+                assignRole()
+            }
+            else if indexPath.row == 1 {
+                revokeRole()
+            }
+            else {
+                delegate.alert(withMessage: "Unexpected Row")
+            }
+        }
+        else if indexPath.section == 2 {
+            if indexPath.row == 0 {
+                // Segue in IB will handle this
+            }
+            else {
+                delegate.alert(withMessage: "Unexpected Row")
+            }
+        }
+        else if indexPath.section == 3 {
+            if indexPath.row == 0 {
+                logout()
+            }
+            else {
+                delegate.alert(withMessage: "Unexpected Row")
+            }
+        }
+        else {
+            delegate.alert(withMessage: "Unexpected Section")
+        }
+    }
+    
+    func registerDeviceToken() {
+        let application = UIApplication.sharedApplication()
+        
+        let tokenId = NSUserDefaults.standardUserDefaults().integerForKey(PhoenixDemoStoredDeviceTokenKey)
+        
+        if tokenId != 0 {
+            let delegate = application.delegate as! AppDelegate
+            delegate.alert(withMessage: "Already Registered!")
+            return
+        }
+        
+        application.registerForRemoteNotifications()
+        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: .Alert, categories: nil))
+    }
+    
+    func unregisterDeviceToken() {
+        let application = UIApplication.sharedApplication()
+        let delegate = application.delegate as! AppDelegate
+        
+        let tokenId = NSUserDefaults.standardUserDefaults().integerForKey(PhoenixDemoStoredDeviceTokenKey)
+        
+        if tokenId == 0 {
+            delegate.alert(withMessage: "Not Registered!")
+            return
+        }
+        
+        PhoenixManager.phoenix.identity.unregisterDeviceToken(withId: tokenId, callback: { (error) -> Void in
+            let notRegisteredError = error?.code == IdentityError.DeviceTokenNotRegisteredError.rawValue
+            
+            if error != nil && !notRegisteredError {
+                delegate.alert(withMessage: "Failed with error: \(error!.code)")
+            }
+            else {
+                NSUserDefaults.standardUserDefaults().removeObjectForKey(PhoenixDemoStoredDeviceTokenKey)
+                NSUserDefaults.standardUserDefaults().synchronize()
+                
+                delegate.alert(withMessage: "Unregister Succeeded!")
+            }
+        })
+    }
+    
+    func assignRole() {
+        let application = UIApplication.sharedApplication()
+        let delegate = application.delegate as! AppDelegate
+        
+        delegate.alert(withMessage: "Not implemented yet")
+    }
+    
+    func revokeRole() {
+        let alertController = UIAlertController(title: "Enter Details", message: nil, preferredStyle: .Alert)
+        
+        alertController.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "RoleId"
+        }
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .Cancel) { (action) -> Void in
+            })
+        
+        alertController.addAction(UIAlertAction(title: "Revoke Role", style: .Default) { [weak self] (action) -> Void in
+            
+            guard let strongSelf = self,
+                roleString = alertController.textFields?.first?.text,
+                roleId = Int(roleString) else {
+                    return
+            }
+            
+            PhoenixManager.phoenix.identity.revokeRole(roleId, user: strongSelf.user!) { (user, error) -> Void in
+                dispatch_async(dispatch_get_main_queue()) {
+                    let application = UIApplication.sharedApplication()
+                    let delegate = application.delegate as! AppDelegate
+                    
+                    if let error = error {
+                        delegate.alert(withMessage: "Failed with error: \(error.code)")
+                    }
+                    else {
+                        delegate.alert(withMessage: "Role Revoked!")
+                    }
+                }
+            }
+        })
+        
+        presentViewController(alertController, animated: true) { }
+    }
+    
+    func logout() {
+        PhoenixManager.phoenix.identity.logout()
+        
+        self.user = nil
+        
+        NSUserDefaults.standardUserDefaults().removeObjectForKey(PhoenixDemoStoredDeviceTokenKey)
+        NSUserDefaults.standardUserDefaults().synchronize()
+        
+        self.performSegueWithIdentifier(UnwindOnLogoutSegue, sender: self)
+    }
+}
